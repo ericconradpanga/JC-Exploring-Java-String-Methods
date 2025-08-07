52d3071d54f23889383d7e2e5b941c76993f33a7
ericconradpanga <ericconrad.pangaiii@gcash.com>
2025-08-07 12:54:34 +0800

    added submission for Hands-on Lab: Exploring Java String Methods

---
+++ JC-Exploring-Java-String-Methods/src/main/java/org/example/StringLab.java
@@ -0,0 +1,121 @@
+package org.example;
+
+public class StringLab {
+    public static void main(String[] args) {
+//        // exercise 1
+//        String greeting = "Hello, Cadets!";
+//        int length = greeting.length();
+//        System.out.println("The length of the string is: " + length);
+//
+//        // prediction: The length of the string is: 14
+//        // observation: The length of the string is: 14
+//
+//        --------------------------------------------------------------
+//
+//        // exercise 2
+//        String message = "Keep learning!";
+//        char character = message.charAt(5);
+//        System.out.println("The character at index 5 is: " + character);
+//
+//        // prediction: The character at index 5 is: l
+//        // observation: The character at index 5 is: l
+//
+//        --------------------------------------------------------------
+//
+//        // exercise 3
+//        String statement = "Java is powerful.";
+//        String part = statement.substring(5, 7);
+//        System.out.println("The extracted substring is: " + part);
+//
+//        // prediction: The extracted substring is: is
+//        // observation: The extracted substring is: is
+
+//        --------------------------------------------------------------
+//
+//        // exercise 4
+//        String mixedCase = "This Is A Test";
+//        String upper = mixedCase.toUpperCase();
+//        String lower = mixedCase.toLowerCase();
+//        System.out.println("Uppercase: " + upper);
+//        System.out.println("Lowercase: " + lower);
+//
+//        // prediction: Uppercase: THIS IS A TEST
+//        //             Lowercase: this is a test
+//
+//        // prediction: Uppercase: THIS IS A TEST
+//        //             Lowercase: this is a test
+
+//        --------------------------------------------------------------
+//
+//        // exercise 5
+//        String sentence = "The quick brown fox jumps over the lazy dog.";
+//        int indexOfFox = sentence.indexOf("fox");
+//        int indexOfZ = sentence.indexOf('z');
+//        int indexOfCat = sentence.indexOf("cat");
+//        System.out.println("Index of 'fox': " + indexOfFox);
+//        System.out.println("Index of 'z': " + indexOfZ);
+//        System.out.println("Index of 'cat': " + indexOfCat);
+//
+//        // prediction: Index of 'fox': 16
+//        //             Index of 'z': 37
+//        //             Index of 'cat': -1
+//
+//        // observation: Index of 'fox': 16
+//        //              Index of 'z': 37
+//        //              Index of 'cat': -1
+//
+//        --------------------------------------------------------------
+//
+//        //  exercise 6
+//        String str1 = "Java";
+//        String str2 = "java";
+//        String str3 = "Java";
+//
+//        boolean isEqual1 = str1.equals(str2);
+//        boolean isEqual2 = str1.equals(str3);
+//        boolean isEqualIgnoreCase = str1.equalsIgnoreCase(str2);
+//
+//        System.out.println("\"Java\".equals(\"java\"): " + isEqual1);
+//        System.out.println("\"Java\".equals(\"Java\"): " + isEqual2);
+//        System.out.println("\"Java\".equalsIgnoreCase(\"java\"): " + isEqualIgnoreCase);
+//
+//        //  prediction: Java.equals(java): false
+//        //  Java.equals(Java): true
+//        //  Java.equalsIgnoreCase(java): true
+//
+//        //  observation: Java.equals(java): false
+//        //  Java.equals(Java): true
+//        //  Java.equalsIgnoreCase(java): true
+//
+//        --------------------------------------------------------------
+//
+//        //  exercise 7
+//        String original = "I like cats. Cats are cute.";
+//        String replaced = original.replace("cats", "dogs");
+//        System.out.println("Original: " + original);
+//        System.out.println("Replaced: " + replaced);
+//
+//        // prediction: Original: I like cats. Cats are cute.
+//        //             Replaced: I like dogs. Cats are cute.
+//
+//        // observation: Original: I like cats. Cats are cute.
+//        //              Replaced: I like dogs. Cats are cute.
+//
+//        --------------------------------------------------------------
+//
+//        //  exercise 8
+//        String padded = "   Lots of spaces   ";
+//        String trimmed = padded.trim();
+//        System.out.println("Padded string length: " + padded.length());
+//        System.out.println("Trimmed string: '" + trimmed + "'");
+//        System.out.println("Trimmed string length: " + trimmed.length());
+//
+//        //  prediction: Padded string length: 20
+//        //              Trimmed string: 'Lots of spaces'
+//        //              Trimmed string length: 14
+//
+//        //  observation: Padded string length: 20
+//        //               Trimmed string: 'Lots of spaces'
+//        //               Trimmed string length: 14
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# JC-Exploring-Java-String-Methods
+# FECP5-1009: Java Certification Preparation
+
+## Hands-on Lab: Exploring Java String Methods
+## Eric Conrad Panga
+## Screenshots of Output
+
+![alt text](1.png) 
+
+![alt text](2.png) 
+
+![alt text](3.png) 
+
+![alt text](4.png) 
+
+![alt text](5.png) 
+
+![alt text](6.png) 
+
+![alt text](7.png) 
+
+![alt text](8.png)
